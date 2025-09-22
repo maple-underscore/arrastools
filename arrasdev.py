644b9b35b7837999c3b933dcfb7ed3227e74607c
maple_ <102273486+maple-underscore@users.noreply.github.com>
2025-09-22 18:22:42 +0000

    rearranged keystrokes

--- arrasdev.py
+++ arrasdev.py
@@ -6,14 +6,11 @@ controller = KeyboardController()
 threads = []
 
 def ballcrashsegment():
-    controller.press("`")
     for _ in range(20):
         for _ in range(20):
             # 390.625 ==> 391 balls (plus 9 for redundancy) ==> 400 balls
             controller.tap("c")
-            controller.tap("h")        
-    controller.release("`")
-
+            controller.tap("h")
 try:
     for i in range(128):
         # 128 threads x 400 balls = 51,200 balls
@@ -28,10 +25,12 @@ try:
         exec(f"print(f'Creating thread {i}')")
         exec(f"threads.append(threading.Thread(target = ballcrashsegment, daemon = True))")
 
+    controller.press("`")
     start = time.time()
     for i in range(128):
         exec(f"print(f'Starting thread {i}')")
         exec(f"threads[{i}].start()")
     print(f"Time taken: {round((time.time()-start)*1000, 3)}ms")
+    controller.release("`")
 except Exception as e:
     print(f"Exception as {e}")
