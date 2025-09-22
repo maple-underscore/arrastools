cb186bd973229464dd99e1b7a577b59692023d25
maple_ <102273486+maple-underscore@users.noreply.github.com>
2025-09-22 18:07:59 +0000

    32 threads => 128 threads 20,000 balls => 50,000 balls

--- arrasdev.py
+++ arrasdev.py
@@ -1,12 +1,9 @@
-import random
 import time
 import threading
-from pynput import keyboard
-from pynput.keyboard import Controller as KeyboardController, Key
-from pynput.mouse import Controller as MouseController, Button
-import tkinter as tk
+from pynput.keyboard import Controller as KeyboardController
 
 controller = KeyboardController()
+threads = []
 
 def ballcrashsegment():
     controller.press("`")
@@ -17,19 +14,24 @@ def ballcrashsegment():
             controller.tap("h")        
     controller.release("`")
 
-threads = []
-start = time.time()
-for i in range(128):
-    # 128 threads x 400 balls = 51,200 balls
-    #    (5,000 balls x 10 ==> 2,500mspt x 10 ==> 25,000mspt?)
-    #    server would probably crash before measurement
-    #
-    # possible side effects:
-    #   maybe socket time out
-    #   possibly the most powerful crash possible
-    
-    exec(f"thread{i} = threading.Thread(target = ballcrashsegment)")
-    exec(f"thread{i}.daemon = True")
-    exec(f"threads.append(thread{i})")
-    exec(f"thread{i}.start()")
-print(f"Time taken: {round((time.time()-start)*1000, 3)}ms")
+try:
+    for i in range(128):
+        # 128 threads x 400 balls = 51,200 balls
+        #    (5,000 balls x 10 ==> 2,500mspt x 10 ==> 25,000mspt?)
+        #    server would probably crash before measurement
+        #
+        # possible side effects:
+        #   maybe socket time out
+        #   possibly the most powerful crash possible
+        #   maybe cant run all 128 threads due to processor limitations?
+        #   irregularity using exec() functions
+        exec(f"print(f'Creating thread {i}')")
+        exec(f"threads.append(threading.Thread(target = ballcrashsegment, daemon = True))")
+
+    start = time.time()
+    for i in range(128):
+        exec(f"print(f'Starting thread {i}')")
+        exec(f"threads[{i}].start()")
+    print(f"Time taken: {round((time.time()-start)*1000, 3)}ms")
+except Exception as e:
+    print(f"Exception as {e}")
